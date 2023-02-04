985fce8ef563a57bb048d81186cb8e04c1e0505a
JiaqiLiFrank <123591501+JiaqiLiFrank@users.noreply.github.com>
2023-02-03 20:12:39 -0500

    Update Lab1 - Has Punctuation.py

--- Lab1 - Has Punctuation.py
+++ Lab1 - Has Punctuation.py
@@ -1,9 +1,3 @@
-# <Jiaqi Li>
-# CS - UY 1121
-# 26 January 2023
-# Lab 1
-# Problem #3
-
 def has_punctuation(sentence):
     has_punctuation = (("'" in sentence) or ("." in sentence) or ("," in sentence) or (";" in sentence) or (":" in sentence) or ("?" in sentence) or ("!" in sentence))
     if (has_punctuation):
